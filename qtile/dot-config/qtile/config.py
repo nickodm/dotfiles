8d8a5fd1a8e7a25c440696084bbaa2ee9ad81bf9
Nicolás Miranda <nmcolivoro@outlook.cl>
2026-07-04 01:52:05 -0400

    Configured layout

--- qtile/dot-config/qtile/config.py
+++ qtile/dot-config/qtile/config.py
@@ -100,7 +100,12 @@ for i in groups:
     )
 
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Columns(
+        border_focus="1e78e4",
+        border_normal="#000000",
+        margin=5,
+        border_width=4
+    ),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
