952a79bea7df85231fc26af8db76f7249a7d0002
Nicolás Miranda <nmcolivoro@outlook.cl>
2026-07-07 21:05:16 -0400

    Disabled bg color in nvim

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -6,3 +6,7 @@ vim.opt.expandtab = true
 
 -- Set relative numbers
 vim.opt.relativenumber = true
+
+-- Disable background color
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'NONE', ctermbg = 'NONE' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'NONE', ctermbg = 'NONE' })
