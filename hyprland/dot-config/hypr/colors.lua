8e112d6ed78427176c81acab2288ece6d2f4977a
Nicolás Miranda <nmcolivoro@outlook.cl>
2026-07-27 03:49:17 -0400

    Added hypr stuff

---
+++ hyprland/dot-config/hypr/colors.lua
@@ -0,0 +1,11 @@
+return {
+    black   = "#000000",
+    white   = "#FFFFFF",
+    red     = "#c02c28",
+    green   = "#2ec27e",
+    yellow  = "#f5c211",
+    blue    = "#1e78e4",
+    magenta = "#9841bb",
+    cyan    = "#0ab9dc",
+    orange  = "#F08623"
+}
---
+++ hyprland/hypr-stow.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/bash
+# Stow all the hypr programs
+
+programs = "hyprland", "hyprlock", "hypridle", "hyprpaper", "hyprsunset"
+
+for program in $programs do
+    echo "$program"
+done
